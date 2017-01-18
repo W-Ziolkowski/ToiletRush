2e47dc53ef3042d088920d03f8ff58760ee34494
Wojciech Ziolkowski <w.ziolkowski.94@gmail.com>
2017-01-18 19:07:41 +0100

    animation update, added destroyer + attempt to spawn the floor

---
+++ Assets/SpawnScrpit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScrpit : MonoBehaviour {
+
+    public GameObject[] obj;
+    public float spawnMin = 1f;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+	}
+	void Spawn() {
+        Instantiate(obj)
+    }
+}
