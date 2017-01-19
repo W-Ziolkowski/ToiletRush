fd07905f244653291490bf920a64f3a7b310fd23
Sergio Manso <sergio.manso@ua.pt>
2017-01-19 23:00:17 +0000

    added menu

---
+++ Assets/ToggleHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleHandler : MonoBehaviour {
+
+	public GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ToggleAudio () {
+        if(playerObject.GetComponent<Toggle>().isOn) {
+            AudioListener.pause = false;
+        }
+        else {
+						AudioListener.pause = true;
+        }
+    }
+}
