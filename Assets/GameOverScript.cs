00c177a46f0f95ba3296576294301afd624e962c
Wojciech Ziolkowski <w.ziolkowski.94@gmail.com>
2017-01-20 09:54:41 +0100

    RC v1.0

--- Assets/GameOverScript.cs
+++ Assets/GameOverScript.cs
@@ -5,11 +5,20 @@ using System.Collections;
 public class GameOverScript : MonoBehaviour {
 
 	int score = 0;
+	int highScore = 0;
 	public Text DisplayScore;
 
 	void Start () {
 		score = PlayerPrefs.GetInt ("Score");
-		DisplayScore.text = "S C O R E : " + score;
+		highScore = PlayerPrefs.GetInt ("HighScore");
+
+		if (score > highScore) {
+			PlayerPrefs.SetInt ("HighScore", score);
+			highScore = PlayerPrefs.GetInt ("HighScore");
+
+		}
+
+		DisplayScore.text = "S C O R E : " + score + "\nHIGHSCORE : " + highScore;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/StartGameScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartGameScript : MonoBehaviour {
+
+	public void OnClick() {
+		SceneManager.LoadScene ("Main", LoadSceneMode.Single);
+	}
+}
